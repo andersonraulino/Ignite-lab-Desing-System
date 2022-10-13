6a81c1fa21e67d12f47672b5127ae488c3d37f08
andersonraulino <anderson.raul4631@gmail.com>
2022-10-13 00:56:25 -0300

    configurando tokens

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ export function App() {
   return (
     <>
 
-      <h1 className='font-bold text-Sxl text-violet-500'>Hello world!</h1>
+      <h1 className='font-bold text-2xl text-violet-500'>Hello world!</h1>
 
-      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+      <button className=' bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
         Enviar
       </button>
 
