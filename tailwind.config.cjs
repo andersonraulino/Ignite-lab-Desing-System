5d7ff8235fa586a909b515fc1f18c6419d20aeca
andersonraulino <anderson.raul4631@gmail.com>
2022-10-15 23:14:43 -0300

    fim aula 2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,36 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
-  theme:{
-
+  content: ["./src/**/*.tsx"],
+  theme: {
     fontSize: {
       xs: 14,
       sm: 16,
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl': 32,
+      "2xl": 32,
     },
 
     colors: {
-    'gray-900': '#121214',
-    'gray-800': '#202024',
-    'gray-400': '#7C7C8A',
-    'gray-100': '#E1E1E6',
-    'gray-200': '#C4C4CC',
-    
-    'cyan-500': '#81D8F7',
-    'cyan-300': '#9BE1FB',
-    
-    'black': '#000000'
-  },
+      transparent: "transparent",
+
+      black: "#000",
+      white: "#fff",
+
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        100: "#E1E1E6",
+        200: "#C4C4CC",
+      },
+
+      cyan: {
+        500: "#81D8F7",
+        300: "#9BE1FB",
+      },
+    },
     extend: {
       fontFamily: {
-        sans: 'Inter, sans-serif'
+        sans: "Inter, sans-serif",
       },
     },
   },
   plugins: [],
-}
+};
