af8b5b2caa514b18d8f0978f01f5e580e7644fc8
andersonraulino <anderson.raul4631@gmail.com>
2022-10-18 22:13:07 -0300

    Começo do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import './styles/global.css'
+import "./styles/global.css";
 
 export function App() {
-
-  return (
-    <>
-
-      <h1 className='font-bold text-2xl text-violet-500'>Hello world!</h1>
-
-      <button className=' bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
-        Enviar
-      </button>
-
-    </>
-  )
+  return;
+  <div className="w-screen h-screen bg-gray-900 flex items-center"></div>;
 }
